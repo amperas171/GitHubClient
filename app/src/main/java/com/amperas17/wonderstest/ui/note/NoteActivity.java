b85a4c1c47ea9d711c3b6b6fe6b38bd8211474e7
amperas17 <amperas17@gmail.com>
2017-08-30 14:08:13 +0300

    permissions for marshmallow

--- app/src/main/java/com/amperas17/wonderstest/ui/note/NoteActivity.java
+++ app/src/main/java/com/amperas17/wonderstest/ui/note/NoteActivity.java
@@ -1,12 +1,14 @@
 package com.amperas17.wonderstest.ui.note;
 
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -20,17 +22,25 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.github.chrisbanes.photoview.PhotoView;
 
 import java.io.File;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnLongClick;
 import butterknife.OnTextChanged;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
 
-public class NoteActivity extends AppCompatActivity implements INoteView {
+public class NoteActivity extends AppCompatActivity implements INoteView, EasyPermissions.PermissionCallbacks {
 
     public static final int GET_CONTENT_TAG = 101;
     public static final int IMAGE_CAPTURE_TAG = 102;
+    private static final int CAMERA_PERM = 123;
+    private static final int READ_EXTERNAL_STORAGE_PERM = 124;
+    private static final int WRITE_EXTERNAL_STORAGE_PERM = 125;
+    private static final int CAMERA_AND_WRITE_EXTERNAL_STORAGE_PERMS = 126;
     public static final String ITEM_KEY = "itemKey";
 
     private INotePresenter presenter;
@@ -60,6 +70,14 @@ public class NoteActivity extends AppCompatActivity implements INoteView {
         initActionBar();
     }
 
+    private void initActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle(getString(R.string.note_to) + " " + presenter.getItemName());
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
     @OnTextChanged(R.id.etTitle)
     protected void onTitleChanged(CharSequence text) {
         presenter.saveNoteTitle(text.toString());
@@ -70,14 +88,29 @@ public class NoteActivity extends AppCompatActivity implements INoteView {
         presenter.saveNoteText(text.toString());
     }
 
+    @AfterPermissionGranted(READ_EXTERNAL_STORAGE_PERM)
     @OnClick(R.id.image)
     protected void onImageClick() {
-        presenter.onImageClick();
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            presenter.onImageClick();
+        } else {
+            EasyPermissions.requestPermissions(this, getString(R.string.rationale_read_storage),
+                    READ_EXTERNAL_STORAGE_PERM, Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
     }
 
+    @AfterPermissionGranted(WRITE_EXTERNAL_STORAGE_PERM | CAMERA_PERM)
     @OnLongClick(R.id.image)
     protected boolean onLongImageClick() {
-        presenter.onImageLongClick();
+        if (EasyPermissions.hasPermissions(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                && EasyPermissions.hasPermissions(this, Manifest.permission.CAMERA)) {
+            presenter.onImageLongClick();
+        } else {
+            String rationale = getString(R.string.rationale_camera) + " and " +
+                    getString(R.string.rationale_write_storage);
+            EasyPermissions.requestPermissions(this, rationale, CAMERA_AND_WRITE_EXTERNAL_STORAGE_PERMS,
+                    Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
         return true;
     }
 
@@ -116,14 +149,6 @@ public class NoteActivity extends AppCompatActivity implements INoteView {
                     .into(image);
         } else {
             image.setImageDrawable(getResources().getDrawable(R.drawable.img_git_folder));
-        }
-    }
-
-    private void initActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setTitle(getString(R.string.note_to) + " " + presenter.getItemName());
-            actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
@@ -169,12 +194,6 @@ public class NoteActivity extends AppCompatActivity implements INoteView {
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        presenter.onDestroy();
-    }
-
-    @Override
     public Parcelable getKeyArg() {
         return getIntent().getParcelableExtra(ITEM_KEY);
     }
@@ -187,5 +206,29 @@ public class NoteActivity extends AppCompatActivity implements INoteView {
     @Override
     public String getTitleText() {
         return etTitle.getText().toString();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.onDestroy();
     }
 }
